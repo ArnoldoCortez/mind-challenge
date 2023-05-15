24c0a84eda614a5de4c8f2300c3a12cf147040dd
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-05-15 09:27:41 -0600

    overwrite mui components styles card and table

---
+++ src/theme/createComponents.ts
@@ -0,0 +1,69 @@
+import { paperClasses, tableCellClasses, PaletteOptions } from "@mui/material";
+
+export function createComponents({ palette }: { palette: PaletteOptions }) {
+  return {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          boxSizing: "border-box",
+        },
+        html: {
+          minHeight: "100vh",
+        },
+        body: {
+          minHeight: "100vh",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: 20,
+          [`&.${paperClasses.elevation1}`]: {
+            boxShadow:
+              "0px 5px 22px rgba(0, 0, 0, 0.04), 0px 0px 0px 0.5px rgba(0, 0, 0, 0.03)",
+          },
+        },
+      },
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          padding: "32px 24px",
+          "&:last-child": {
+            paddingBottom: "32px",
+          },
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          borderBottomColor: palette.divider,
+          padding: "15px 16px",
+        },
+      },
+    },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          borderBottom: "none",
+          [`& .${tableCellClasses.root}`]: {
+            borderBottom: "none",
+            backgroundColor: palette.neutral[50],
+            color: palette.neutral[700],
+            fontSize: 12,
+            fontWeight: 600,
+            lineHeight: 1,
+            letterSpacing: 0.5,
+            textTransform: "uppercase",
+          },
+          [`& .${tableCellClasses.paddingCheckbox}`]: {
+            paddingTop: 4,
+            paddingBottom: 4,
+          },
+        },
+      },
+    },
+  };
+}
