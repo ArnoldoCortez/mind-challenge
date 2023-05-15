401dc5df008f3846bc06d5c7a962c2da6abeba6f
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2023-05-15 09:31:53 -0600

    created team logs page

---
+++ src/pages/TeamLogs/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+function TeamLogsPage() {
+  return (
+    <Container maxWidth="xl" component="main" sx={{ py: 8 }}>
+      <Typography component="h1" variant="h4">
+        Team Logs
+      </Typography>
+    </Container>
+  );
+}
+
+export default TeamLogsPage;
